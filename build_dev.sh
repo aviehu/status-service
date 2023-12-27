45ed7da8f4574a87f3a39e73bb719743e99c7654
Aviehu <aviehu@gmail.com>
2023-12-27 18:56:30 +0200

    build sh changes

--- build_dev.sh
+++ build_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 COMMIT=`git rev-parse --short HEAD`
-TAG="rc-status-service-$COMMIT"
+TAG="rc-$COMMIT"
 
 docker build -t 788139694487.dkr.ecr.eu-west-1.amazonaws.com/status-service:$TAG .
 docker push 788139694487.dkr.ecr.eu-west-1.amazonaws.com/status-service:$TAG
